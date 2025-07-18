1afa8edda18508e5aa1b9a25dcf88e3691ba912a
ARKADEEP HALDER <164157560+arka-lsik@users.noreply.github.com>
2025-07-18 17:21:17 +0530

    Update testbench.v

--- Detailed Design/testbench.v
+++ Detailed Design/testbench.v
@@ -1,42 +1,51 @@
 module TB_Adder();
+
   reg [31:0] test32_in1;
   reg [31:0] test32_in2;
-  reg test32_cin,test_clk;
-  reg [31:0] test_in1,test_in2;
-  wire [31:0] test32_sum;reg [31:0] test1,test2;
+  reg test32_cin;
+  reg test_clk;
+
+  wire [31:0] test32_sum;
   wire test32_cout;
-  Adder dut32(.TClk(test_clk),.ra(test32_in1), .rb(test32_in2), .cin(test32_cin), .Sum(test32_sum), .Cout(test32_cout));
 
-  initial
-    begin
+  integer i;  // Declare integer outside procedural blocks
 
-      repeat (1000)
-        begin
-            
-            test1=$random;
-            test2=$random;
-            test_in1=test1;
-          	test_in2=test2;
-      		    test32_cin = $random % 2;
-          repeat(4)
-            begin
-           		test32_in1 = test_in1 % 256;
-        		test32_in2 = test_in2 % 256;
-              
-              	@(posedge test_clk);
-              	test_in1 = test_in1>>8;
-              	test_in2 = test_in2>>8;
-          		#100;
-            end
-        end
-        #1000 $finish;
+  // Instantiate DUT
+  Adder dut32(
+    .TClk(test_clk),
+    .ra(test32_in1),
+    .rb(test32_in2),
+    .cin(test32_cin),
+    .Sum(test32_sum),
+    .Cout(test32_cout)
+  );
+
+  // Clock generation
+  initial begin
+    test_clk = 0;
+    forever #5 test_clk = ~test_clk;
+  end
+
+  // Apply random stimulus
+  initial begin
+    for (i = 0; i < 1000; i = i + 1) begin
+      @(negedge test_clk);
+      test32_in1 = $random;
+      test32_in2 = $random;
+      test32_cin = $random % 2;
+      @(posedge test_clk);
+      #10;
     end
-  //end of initial block
+    #100 $finish;
+  end
 
-  initial
-    begin
-      $monitor($time, " IN1:%b, IN2:%b, CIN:%b, SUM:%b, COUT:%b", test1, test2, test32_cin, test32_sum, test32_cout);
-      $dumpfile("fulladderN.vcd");
-      $dumpvars;
-    end
+  // Monitor output
+  initial begin
+    $monitor("Time=%0t | IN1=%h IN2=%h CIN=%b | SUM=%h COUT=%b",
+      $time, test32_in1, test32_in2, test32_cin, test32_sum, test32_cout);
+    $dumpfile("adder_test.vcd");
+    $dumpvars(0, TB_Adder);
+  end
+
 endmodule
+
